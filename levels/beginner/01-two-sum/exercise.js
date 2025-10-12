cded8050c88c4d3a47c045d6f02cce387e7484ef
michelleMoncayo29 <110497050+michelleMoncayo29@users.noreply.github.com>
2025-10-12 15:03:13 -0400

    ejercicio 1

--- levels/beginner/01-two-sum/exercise.js
+++ levels/beginner/01-two-sum/exercise.js
@@ -1,23 +1,28 @@
 /**
  * Two Sum
- * 
+ *
  * Descripción: Dado un array de enteros nums y un entero target, devuelve los índices de los dos números que suman target.
  * Dificultad: BEGINNER
- * 
+ *
  * @param {number[]} nums - Array de números enteros
  * @param {number} target - Número objetivo
  * @returns {number[]} Array con los índices de los dos números que suman target
- * 
+ *
  * Ejemplo:
  * twoSum([2, 7, 11, 15], 9) // [0, 1]
  */
 
 function twoSum(nums, target) {
-    // TODO: Implementar la solución aquí
-    
-    // Pista: Puedes usar un hash map para almacenar los números vistos junto con sus índices
-    
-    throw new Error('Función no implementada');
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
+  return [];
 }
+
+console.log(twoSum([3, 2, 4], 6)); // [0, 1]
 
 module.exports = twoSum;
