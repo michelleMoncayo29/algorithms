fdf60b0e8a3f27d4831aa4a08c08f3aa4dafbf37
michelleMoncayo29 <110497050+michelleMoncayo29@users.noreply.github.com>
2026-03-07 21:53:21 -0400

    intento

--- levels/01-beginner/58-api-comments/exercise.js
+++ levels/01-beginner/58-api-comments/exercise.js
@@ -13,9 +13,26 @@
  */
 async function fetchCommentsByPost(postId) {
   // Tu código aquí
-  // 1. Obtener datos de la API usando el parámetro postId
-  // 2. Retornar el array de comentarios
+  try {
+    // 1. Obtener datos de la API usando el parámetro postId
+    const consult = await fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+    
+    if (!consult.ok) {
+      throw new Error(`Failed to fetch user`);
+    }
+
+    let consultJson = await consult.json();
+
+    // 2. Retornar el array de comentarios
+    // console.log('primera función', consultJson);
+
+    return consultJson;
+  } catch (messenge) {
+    throw new Error('Network Error');
+  }
 }
+
+// fetchCommentsByPost(1);
 
 /**
  * Obtiene solo los emails de los usuarios que comentaron en un post.
@@ -26,8 +43,13 @@ async function fetchCommentsByPost(postId) {
 async function getCommentersEmails(postId) {
   // Tu código aquí
   // 1. Llamar a fetchCommentsByPost(postId)
+  const postComment = await fetchCommentsByPost(postId);
   // 2. Mapear el resultado para obtener solo la propiedad 'email'
+  const comment = postComment.map(post => post.email);
+  // console.log('✅✅✅✅✅✅', comment);
+  return comment;
 }
+// getCommentersEmails(1);
 
 /**
  * Cuenta los comentarios por dominio de nivel superior (.com, .net, etc.).
@@ -39,9 +61,21 @@ async function getCommentersEmails(postId) {
 async function countCommentsByDomain(postId) {
   // Tu código aquí
   // 1. Llamar a getCommentersEmails(postId)
-  // 2. Extraer el dominio (parte después del último punto) de cada email
+  const commentDomain = await getCommentersEmails(postId);
+
+  return commentDomain.reduce((acc, email) => {
+    // 2. Extraer el dominio (parte después del último punto) de cada email
+    const tld = email.split('.').pop();
+    
+    // Inicializar a 0 si no existe y sumar 1
+    acc[tld] = (acc[tld] || 0) + 1;
+    
+    return acc;
+  }, {});
   // 3. Contar cuántos hay de cada uno (usando un objeto o reduce)
 }
+
+// countCommentsByDomain(1);
 
 /**
  * Obtiene el post y sus comentarios combinados.
@@ -73,6 +107,14 @@ async function searchComments(query) {
   // Tu código aquí
   // 1. Hacer petición fetch con el parámetro q
   // 2. Retornar los resultados
+
+  const response = await fetch(`https://jsonplaceholder.typicode.com/comments?q=${query}`);
+  
+  if (!response.ok) {
+    throw new Error(`Error HTTP: ${response.status}`);
+  }
+  
+  return response.json();
 }
 
 module.exports = {
